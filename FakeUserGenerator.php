c3b4023d64f078819be016630b695b16fe4e23b6
Ohffs <ohffsnotnow@gmail.com>
2024-10-31 09:28:01 +0000

    feat: add methods to generate staff and student users
    - Update documentation to specify generating staff users
    - Implement `generateStaff` method to create staff users
    - Implement `generateStudent` method to create student users
    - Generate unique usernames and emails for student users

--- FakeUserGenerator.php
+++ FakeUserGenerator.php
@@ -7,7 +7,7 @@ use Faker\Factory;
 class FakeUserGenerator
 {
     /**
-     * Generate a user or a collection of users.
+     * Generate a staff user or a collection of staff users.
      * @param int $count
      * @return array|\Illuminate\Support\Collection
      */
@@ -20,6 +20,40 @@ class FakeUserGenerator
             $forename = $faker->unique()->firstName();
             $username = strtolower($forename[0] . $faker->randomLetter() . $surname[0] . random_int(1, 9) . $faker->randomLetter());
             $email = ucfirst($forename) . '.' . ucfirst($surname) . '@example.ac.uk';
+            $users[] = [
+                'surname' => $surname,
+                'forenames' => $forename,
+                'username' => $username,
+                'email' => $email,
+            ];
+        }
+
+        if ($count == 1) {
+            return $users[0];
+        }
+
+        return collect($users);
+    }
+
+    public static function generateStaff(int $count = 1): array|\Illuminate\Support\Collection
+    {
+        return self::generate($count);
+    }
+
+    /**
+     * Generate a student user or a collection of student users.
+     * @param int $count
+     * @return array|\Illuminate\Support\Collection
+     */
+    public static function generateStudent(int $count = 1): array|\Illuminate\Support\Collection
+    {
+        $faker = Factory::create();
+        $users = [];
+        for ($i = 0; $i < $count; $i++) {
+            $surname = $faker->unique()->lastName();
+            $forename = $faker->unique()->firstName();
+            $username = random_int(1000000, 9999999) . strtolower($surname[0]);
+            $email = $username . '@student.example.ac.uk';
             $users[] = [
                 'surname' => $surname,
                 'forenames' => $forename,
